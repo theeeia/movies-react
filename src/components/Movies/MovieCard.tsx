328c33e422f7bfe426237c0517ecc106c5ee38a3
theeeia <thea.dimovska@hotmail.com>
2022-07-28 23:28:43 +0200

    mapped movies to display in cards

--- src/components/Movies/MovieCard.tsx
+++ src/components/Movies/MovieCard.tsx
@@ -2,24 +2,24 @@ import { ReactComponent as StarIcon } from "../../assets/images/star.svg";
 import { ReactComponent as HeartIcon } from "../../assets/images/heart.svg";
 
 function MovieCard(props: any) {
-  const { className, genre, title, year, language, length } = props;
+  const { genre, title, year, language, poster, starsNumber } = props;
+  const stars = [];
+  for (let i = 0; i < starsNumber; i++) stars.push(<StarIcon />);
 
   return (
-    <div className={className}>
+    <div className="col-12 col-md-6 col-lg-4 col-xl-3">
       <div className="movie-card">
-        <div className="movie-card__image">
+        <div className="movie-card__image-box">
           <div className="movie-card__genre">{genre}</div>
+          <img className="movie-card__image" src={`https://image.tmdb.org/t/p/w500/${poster}`} />
         </div>
         <div className="movie-card__details">
           <div className="movie-card__title">{title}</div>
           <div className="movie-card__info">
             <p>{year}</p>
-            <p>{language}</p>
-            <p>{length}</p>
+            <p className="txt--uppercase">{language}</p>
           </div>
-          <div className="movie-card__rating">
-            <StarIcon /> <StarIcon /> <StarIcon /> <StarIcon />
-          </div>
+          <div className="movie-card__rating">{stars}</div>
         </div>
 
         <div className="movie-card__favorite">
--- src/pages/Movies/NowPlaying.tsx
+++ src/pages/Movies/NowPlaying.tsx
@@ -1,37 +1,33 @@
-// Components
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+// Components
+import Loader from "../../components/Loader/Loader";
 import MovieCard from "../../components/Movies/MovieCard";
 import MoviesHeader from "../../components/Movies/MoviesHeader";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
+
 import { API_ENDPOINT_BASE, API_KEY } from "../../config/config";
 import handleFetchCall from "../../utils/handleFetchCall";
 
 function NowPlaying() {
   const { handleFetch } = handleFetchCall();
 
-  const queryMultiple = () => {
-    const genres = useQuery(["genres"], () =>
-      handleFetch(`${API_ENDPOINT_BASE}genres/get-movie-list`, "GET"),
-    );
-    const movies = useQuery(["movies"], () =>
-      handleFetch(
-        `${API_ENDPOINT_BASE}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
-        "GET",
-      ),
-    );
-    return [genres, movies];
-  };
+  const { status: statusGenres, data: genres } = useQuery(["genres"], () =>
+    handleFetch(`${API_ENDPOINT_BASE}/genre/movie/list?api_key=${API_KEY}&language=en-US`, "GET"),
+  );
+  const { status: statusMovies, data: movies } = useQuery(["movies"], () =>
+    handleFetch(
+      `${API_ENDPOINT_BASE}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+      "GET",
+    ),
+  );
 
-  useEffect(() => {
-    // if (!data || !Object.entries(data).length) return;
-    // // Load the data from the request when it arrives and fill out the form
-    // console.log(data);
-    // console.log(data.results[0].original_title);
-    // console.log(data.results[0].original_language);
-    // console.log(data.results[0].release_date);
-    // genre, title, year, language, length
-  }, [status]);
+  const getStarsNumber = (rating: number) => {
+    if (rating > 8) return 5;
+    else if (rating <= 8 && rating > 6) return 4;
+    else if (rating <= 6 && rating > 4) return 3;
+    else if (rating <= 4 && rating > 2) return 2;
+    else if (rating <= 2) return 1;
+  };
 
   return (
     <>
@@ -40,14 +36,29 @@ function NowPlaying() {
         <div className="breadcrumbs">Home</div>
         <MoviesHeader title="Now Playing" />
 
-        <div className="row">
-          <MovieCard className="col-3" />
-          <MovieCard className="col-3" />
-          <MovieCard className="col-3" />
-          <MovieCard className="col-3" />
-          <MovieCard className="col-3" />
-          <MovieCard className="col-3" />
-        </div>
+        {statusGenres === "success" && statusMovies === "success" ? (
+          <div className="row mt--30">
+            {movies.results.map((movie: any) => {
+              const genre = genres.genres.filter(
+                (genre: any) => genre.id === movie.genre_ids[0],
+              )[0];
+
+              return (
+                <MovieCard
+                  key={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.title}
+                  year={movie.release_date.split("-")[0]}
+                  language={movie.original_language}
+                  genre={genre.name}
+                  starsNumber={getStarsNumber(movie.vote_average)}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
