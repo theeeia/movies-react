08e0e35fe541b2d8f12c9af4ee7ff8a2012068cc
Teodora Dimovska <teodorad@codeart.mk>
2022-08-08 19:36:43 +0200

    added comments to Favorites page

--- src/pages/Movies/Favorites.tsx
+++ src/pages/Movies/Favorites.tsx
@@ -4,12 +4,15 @@ import { API_ENDPOINT_BASE, API_KEY } from "../../config/config";
 import handleFetchCall from "../../utils/handleFetchCall";
 
 const Favorites = () => {
+  // Read list from local storage
   const favoritesList = localStorage.getItem("favoritesList")
     ? JSON.parse(localStorage.getItem("favoritesList") || "")
     : [];
 
   const { handleFetch } = handleFetchCall();
   let favoriteMoviesQueries: any = [];
+
+  // Get movie details for every movie in the list
   if (favoritesList.length != 0) {
     favoriteMoviesQueries = useQueries({
       queries: favoritesList.map((movieId: number) => {
@@ -25,28 +28,12 @@ const Favorites = () => {
     });
   }
 
-  let favoriteMoviesActors: any = [];
-  if (favoritesList.length != 0) {
-    favoriteMoviesActors = useQueries({
-      queries: favoritesList.map((movieId: number) => {
-        return {
-          queryKey: ["favorite-actors", movieId],
-          queryFn: () =>
-            handleFetch(
-              `${API_ENDPOINT_BASE}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`,
-              "GET",
-            ),
-        };
-      }),
-    });
-  }
-
+  // set to true if any query is still loading
   const isLoading = favoriteMoviesQueries.some((query: any) => query.isLoading);
-  const isLoadingActors = favoriteMoviesActors.some((query: any) => query.isLoading);
 
-  if (!isLoading && !isLoadingActors) {
+  // Print movies if all are loaded
+  if (!isLoading) {
     console.log({ favoriteMoviesQueries });
-    console.log({ favoriteMoviesActors });
   }
 
   return <div>{isLoading}</div>;
