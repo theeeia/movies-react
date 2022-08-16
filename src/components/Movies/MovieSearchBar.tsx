7f0db6b9d8a0e0b7c186fcf9f99f24a20c222cb8
Teodora Dimovska <teodorad@codeart.mk>
2022-08-16 16:23:31 +0200

    added new searchInput component and fixed style

--- src/components/Movies/MovieSearchBar.tsx
+++ src/components/Movies/MovieSearchBar.tsx
@@ -7,14 +7,14 @@ import { MoviesSearchBarProps, SortValueTypes } from "./interfaces";
 import { DropdownItemProps } from "../Dropdown/interfaces";
 
 // Statics
-import { MOVIES_DROPDOWN_SORT_ITEMS, MOVIES_SEARCH_FILTER_ITEMS } from "../../pages/Movies/statics";
+import { MOVIES_DROPDOWN_SORT_ITEMS } from "../../pages/Movies/statics";
 
 // Components
 import Dropdown from "../Dropdown/Dropdown";
 
 // Icons
-import { ReactComponent as DropdownArrow } from "../../assets/images/dropdown-arrow.svg";
-import SearchDropdown from "../Dropdown/SearchDropdown";
+
+import MovieSearchInput from "./MovieSearchInput";
 
 const MovieSearchBar = ({
   title,
@@ -37,26 +37,13 @@ const MovieSearchBar = ({
       <h4 className="search-bar__title">{title}</h4>
 
       <div className="search-bar__search">
-        <div className="search-bar__search-box">
-          <SearchIcon />
-          <span>
-            <input
-              className="search-bar__input"
-              placeholder="Search titles here..."
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                handleSearch(event.currentTarget.value)
-              }
-            />
-          </span>
+        <MovieSearchInput
+          icon={<SearchIcon />}
+          modifierClass=""
+          handleInputChange={handleSearch}
+          handleDropdownItem={handleFilterDropdownItem}
+        />
 
-          <span className="search-bar__dropdown">
-            <SearchDropdown
-              icon={<DropdownArrow />}
-              items={MOVIES_SEARCH_FILTER_ITEMS}
-              handleDropdownItem={handleFilterDropdownItem}
-            />
-          </span>
-        </div>
         <div className="search-bar__filter">
           <Dropdown
             title="Sorting"
