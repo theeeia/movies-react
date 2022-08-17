01e136baf7ddea51b664618549aa961776344ea4
Teodora Dimovska <teodorad@codeart.mk>
2022-08-17 18:45:02 +0200

    fixed checkbox style

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -3,16 +3,16 @@ import { CheckboxProps } from "./interfaces";
 const Checkbox = ({ id, label, name, type, checked, onChange }: CheckboxProps) => {
   return (
     <div className="checkbox">
+      <input
+        value={id}
+        type={type}
+        name={name}
+        checked={checked}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          onChange(event.currentTarget.value)
+        }
+      />
       <label className="checkbox__box">
-        <input
-          value={id}
-          type={type}
-          name={name}
-          checked={checked}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            onChange(event.currentTarget.value)
-          }
-        />
         <span></span>
         {label}
       </label>
