082eb28371bb27369d29b8163ebd4b699d63565b
michiel <thea.dimovska@hotmail.com>
2024-10-28 22:48:06 +0100

    custom 404 page

--- src/components/Reroute/Reroute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const Reroute = () => {
-    console.log("reroute")
-    return <Navigate to="/movies/now-playing" replace />;
-};
-
-export default Reroute;
